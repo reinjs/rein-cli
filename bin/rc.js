40551645bcd32d7c6519a940c8a4c00584c0975e
shenyunjie <shenyunjie@u51.com>
2018-05-21 15:39:44 +0800

    update cli

--- bin/rc.js
+++ bin/rc.js
@@ -12,6 +12,7 @@ const app = new program(path.resolve(__dirname, '..', 'lib'));
 
 app
   .version(pkg.version)
+  .setMaxListeners(1000)
   .on('404', NotFoundEvent(app))
   .on('error', ErrorEvent(app))
   .on('exit', ExitEvent(app))
@@ -19,6 +20,14 @@ app
 
 app.command('new :name([a-z0-9_\\-]+)?', 'middleware/open-spinner', 'controller/create-app');
 app.command('add :name([a-zA-Z0-9_\\-\/\.]+)', 'middleware/find-root', 'middleware/find-plugin-addones', 'middleware/open-spinner', 'controller/add-module');
+app.command('plugin :name([a-z0-9_\\-]+)?', 'middleware/open-spinner', 'controller/create-plugin');
+
+/**
+ * 支持插件自定义命令调用
+ * 以上命令都不匹配的情况下会新建router
+ * 根据设定的router规则
+ * 匹配到执行的函数
+ */
 app.command('*', 'middleware/find-root', 'middleware/find-plugin-addones', 'middleware/open-spinner', 'middleware/resolve-plugin');
 
 app.listen();
